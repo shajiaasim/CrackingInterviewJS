912cf827370f7f28e0ccbf9eb814ae0df4e9be40
aasimshaji <shajiaasim@gmail.com>
2018-07-15 12:00:19 -0500

    Beautify

--- Chapter 2/palindrome_2_6.js
+++ Chapter 2/palindrome_2_6.js
@@ -1,58 +1,58 @@
-function LinkedList (val){
-this.value=val;
-this.next = null;
+function LinkedList(val) {
+  this.value = val;
+  this.next = null;
 }
 
-LinkedList.prototype.add = function (node){
+LinkedList.prototype.add = function(node) {
 
-var newNode = new LinkedList(node.value);
-var currentNode = this;
+  var newNode = new LinkedList(node.value);
+  var currentNode = this;
 
-if (this.value==null) this.value=node.value;
-else{
+  if (this.value == null) this.value = node.value;
+  else {
 
-while (currentNode.next!=null){
-currentNode = currentNode.next;
-}
+    while (currentNode.next != null) {
+      currentNode = currentNode.next;
+    }
 
-currentNode.next = newNode;
-
-
-}
+    currentNode.next = newNode;
 
 
-}
+  }
 
 
-function isPalindrome(head){
-
-var obj = [];
-var originalList= head;
-
-
-var reversedList = new LinkedList(null);
-
-while (originalList!=null){
-obj.push(originalList);
-originalList = originalList.next;
-}
-
-for (var i =obj.length-1; i>=0; i-- ){
-reversedList.add(obj[i]);
 }
 
 
+function isPalindrome(head) {
 
-while (reversedList!=null){
+  var obj = [];
+  var originalList = head;
 
-if(head.value!=reversedList.value) return false;
-else {
-head=head.next;
-reversedList= reversedList.next;
-}
-}
 
-return true;
+  var reversedList = new LinkedList(null);
+
+  while (originalList != null) {
+    obj.push(originalList);
+    originalList = originalList.next;
+  }
+
+  for (var i = obj.length - 1; i >= 0; i--) {
+    reversedList.add(obj[i]);
+  }
+
+
+
+  while (reversedList != null) {
+
+    if (head.value != reversedList.value) return false;
+    else {
+      head = head.next;
+      reversedList = reversedList.next;
+    }
+  }
+
+  return true;
 
 }
 
