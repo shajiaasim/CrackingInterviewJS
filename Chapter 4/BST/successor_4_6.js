eccd1f74652e91d2be032a6a9cc4e36f92990b65
aasimshaji <shajiaasim@gmail.com>
2018-07-15 17:50:10 -0500

    Successor

---
+++ Chapter 4/BST/successor_4_6.js
@@ -0,0 +1,21 @@
+
+// My solution- maybe wrong
+function getSuccessor(rootNode, getSuccessorNode, rootNodeFound) {
+
+
+  if (!rootNode) return;
+
+
+
+  getSuccessor(rootNode._left, getSuccessorNode, rootNodeFound);
+
+  if (rootNode._val == getSuccessorNode._val) {
+
+    if (rootNode._right) getSuccessor(rootNode._right, getSuccessorNode, true)
+  } else if (rootNodeFound) {
+    console.log(rootNode._val);
+  }
+
+  getSuccessor(rootNode._right, getSuccessorNode, rootNodeFound);
+
+}
