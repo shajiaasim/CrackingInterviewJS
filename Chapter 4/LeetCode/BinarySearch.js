8275d946a526dc4305e658f50c32797e6e3d0429
aasimshaji <shajiaasim@gmail.com>
2018-07-15 12:32:26 -0500

    K to last

--- Chapter 4/LeetCode/BinarySearch.js
+++ Chapter 4/LeetCode/BinarySearch.js
@@ -21,21 +21,29 @@ function binarySearch(nums, target) {
 //While loop
 
 
-function binarchSearchIteration(nums, target) {
+function binarySearchIteration(nums, target) {
 
 
   var low = 0;
   var high = nums.length - 1;
 
   while (low < high) {
-    var mid = Math.floor((high - low) / 2);
+    var mid = Math.round((high + low) / 2);
 
     if (target == nums[mid]) return mid;
 
-    if (target < nums[mid]) high = mid;
-    else low = mid;
+    if (target < nums[mid]) high = mid-1;
+    else low = mid+1;
 
   }
 
-
+return -1;
 }
+
+
+var nums = [1,2,3,4,5,6,7,8,9]
+
+var nums2 = [1,2,3,4,5,6,7,8,9,10]
+
+
+console.log(binarySearchIteration(nums2, 8));
